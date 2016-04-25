b064f5f989a9f89284c96ceb8b36751fca1d590b
Jerome Baudoux <jerome.baudoux@mirakl.com>
2016-04-25 13:08:10 +0200

    Add router and question card page

---
+++ src/app/components/question-card/question-card.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'question-card',
+  templateUrl: './app/components/question-card/question-card.html'
+})
+export class QuestionCard {
+
+}
--- src/app/components/theme-card/theme-card.ts
+++ src/app/components/theme-card/theme-card.ts
@@ -1,11 +1,15 @@
 import { Component, Input, AfterViewInit, ViewEncapsulation } from 'angular2/core';
 import { ITechnology } from '../../services/technologies-store/technologies-store';
 
+import {ROUTER_DIRECTIVES, RouterLink} from 'angular2/router';
+
+
 @Component({
   selector: 'theme-card',
   templateUrl: './app/components/theme-card/theme-card.html',
   styleUrls: ['./styles/card.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  directives: [ROUTER_DIRECTIVES, RouterLink]
 })
 export class ThemeCard implements AfterViewInit {
   @Input() theme : ITechnology;
